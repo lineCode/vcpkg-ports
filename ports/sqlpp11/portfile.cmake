afc2854e60e0cf22adca62a3c2bd9e7bd362f400
Dirk Vanden Boer <dirk.vdb@gmail.com>
2018-06-15 17:04:05 +0200

    ceres sqlite3 sqlpp11 added

---
+++ ports/sqlpp11/portfile.cmake
@@ -0,0 +1,43 @@
+set(VERSION_MAJOR 0)
+set(VERSION_MINOR 56)
+set(VERSION ${VERSION_MAJOR}.${VERSION_MINOR})
+
+include(vcpkg_common_functions)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO rbock/sqlpp11
+    REF ${VERSION}
+    SHA512 94b8e0a91f52dfcf79d4d4ef15fe205afa026d20013aa691d39fad690417ff64e53eae9749149f99d860f1bc5b3c1c66cffb150d097ff8aa3c6c33fd1277ed30
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DENABLE_TESTS=OFF
+)
+
+vcpkg_install_cmake()
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/sqlpp11.dll ${CURRENT_PACKAGES_DIR}/bin/sqlpp11.dll)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/sqlpp11d.dll ${CURRENT_PACKAGES_DIR}/debug/bin/sqlpp11d.dll)
+endif()
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share)
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/share)
+file(RENAME ${CURRENT_PACKAGES_DIR}/lib/cmake/Sqlpp11 ${CURRENT_PACKAGES_DIR}/share/sqlpp11)
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/cmake/Sqlpp11 ${CURRENT_PACKAGES_DIR}/debug/share/sqlpp11)
+vcpkg_fixup_cmake_targets()
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools)
+file(RENAME ${CURRENT_PACKAGES_DIR}/bin/sqlpp11-ddl2cpp ${CURRENT_PACKAGES_DIR}/tools/sqlpp11-ddl2cpp)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+
+vcpkg_copy_pdbs()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
